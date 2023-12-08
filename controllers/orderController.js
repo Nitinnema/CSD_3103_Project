d086c4733d3086c30abe7f2180450c12cb9387eb
Nitin Nema <nitinnema612@gmail.com>
2023-12-08 02:58:20 -0500

    cart functionality

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,9 +2,8 @@ let cart = []; // In-memory cart storage
 
 // Add book to cart
 const addToCart = (req, res) => {
-    const { bookId, title, price } = req.body; // Assuming you send these details
-    const book = { bookId, title, price };
-    
+    const { bookId, title, price, quantity } = req.body; // Assuming you send these details
+    const book = { bookId, title, price, quantity };
     cart.push(book);
     res.status(200).send('Book added to cart!');
 };
@@ -14,7 +13,18 @@ const getCart = (req, res) => {
     res.json(cart);
 };
 
+const removeFromArrayById = (array, id) => {
+    return array.filter(item => item.bookId !== id);
+};
+
+const removeFromCart = (req, res) => {
+    const newArray = removeFromArrayById(cart, req.params.id);
+    cart = newArray;
+    res.status(200).send('Book removed to cart!');
+}
+
 module.exports = {
     addToCart,
     getCart,
+    removeFromCart,
 };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,5 +4,6 @@ const orderController = require('../controllers/orderController');
 
 orderRouter.post('/add-to-cart', orderController.addToCart);
 orderRouter.get('/getBooks', orderController.getCart);
+orderRouter.delete('/remove-from-cart/:id', orderController.removeFromCart);
 
 module.exports = orderRouter;
